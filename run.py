45775c12604c5360f7f40638f9f3e3cbf0ba469b
master malach <malekiciw@gmail.com>
2014-04-24 17:19:51 +0430

    nothing

--- run.py
+++ run.py
@@ -8,8 +8,12 @@ from cherryblog import start
 
 import sys
 import argparse
+import os.path
+thisdir = os.path.dirname(__file__)
+config_path = os.path.abspath(os.path.join(thisdir,'development.conf'))
 parser = argparse.ArgumentParser(description='Simple blogging system, using cherrypy')
-parser.add_argument('-c','--config-file', dest='config_files', nargs='?', action='append', default=[],help='YAML configuration file')
+
+parser.add_argument('-c','--config-file', dest='config_files', nargs='?', action='append', default=[config_path],help='YAML configuration file')
 
 if __name__ == '__main__':
     args = parser.parse_args()
